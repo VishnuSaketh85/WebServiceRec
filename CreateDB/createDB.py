36fea2774ecf6da9058a67ebeebf7c736d5202d8
VishnuSaketh85 <60757948+VishnuSaketh85@users.noreply.github.com>
2021-03-26 19:23:58 -0700

    test
    Signed-off-by: VishnuSaketh85 <60757948+VishnuSaketh85@users.noreply.github.com>

--- CreateDB/createDB.py
+++ CreateDB/createDB.py
@@ -7,6 +7,7 @@ def create_db(cursor):
     create_user_table(cursor)
     create_ws_table(cursor)
 
+
 def create_user_table(cursor):
     create_query = "CREATE TABLE IF NOT EXISTS USERS(" \
                    "user_id INT PRIMARY KEY," \
@@ -33,10 +34,31 @@ def create_ws_table(cursor):
                    "ip_no VARCHAR(50)," \
                    "autonomous_systems TEXT," \
                    "lat TEXT," \
-                   "long TEXT)"
+                   "long TEXT);" \
+                   "CREATE TABLE IF NOT EXISTS WebServices(" \
+                   "service_id INT PRIMARY KEY," \
+                   "wsdl_address TEXT," \
+                   "service_provider TEXT," \
+                   "ip_address VARCHAR(100)," \
+                   "country VARCHAR(50)," \
+                   "ip_no VARCHAR(50)," \
+                   "autonomous_systems TEXT," \
+                   "lat TEXT," \
+                   "long TEXT);"
     cursor.execute(create_query)
 
-    copy_query = "COPY webservices FROM '" + cwd + "/wslist.txt" + "' WITH (FORMAT csv, HEADER true, DELIMITER E'\t')"
-
-
+    copy_query = "COPY webservices_temp FROM '" + cwd + "/wslist.txt" + \
+                 "' WITH (FORMAT csv, HEADER true, DELIMITER E'\t')"
     cursor.execute(copy_query)
+
+    alter_query = "INSERT INTO webservices (SELECT DISTINCT ON(service_id) service_id, wsdl_address," \
+                  "service_provider, ip_address, country, ip_no, autonomous_systems, lat, " \
+                  "long from webservices_temp);" \
+                  "DROP TABLE webservices_temp;" \
+                  "ALTER TABLE webservices add category VARCHAR(100)" \
+                  "ALTER TABLE webservices" \
+                  "ALTER COLUMN long TYPE float USING NULLIF(long, 'null')::float;" \
+                  "ALTER TABLE webservices" \
+                  "ALTER COLUMN lat TYPE float USING NULLIF(lat, 'null')::float;"
+
+    cursor.execute(alter_query)
--- CreateDB/main.py
+++ CreateDB/main.py
@@ -4,17 +4,21 @@ from CreateDB.createDB import create_db
 
 
 def main():
-    username = input("Enter the user name : ")
-    password = input("Enter the password : ")
-    host = input("Enter host: ")
-    port = input("Enter port: ")
-    database = input("Enter database name: ")
-    connection = psycopg2.connect(user=username,
-                                  password=password,
-                                  host=host,
-                                  port=port,
-                                  database=database)
-
+    # useranme = input("Enter the user name : ")
+    # password = input("Enter the password : ")
+    # host = input("Enter host: ")
+    # port = input("Enter port: ")
+    # database = input("Enter database name: ")
+    # connection = psycopg2.connect(user=username,
+    #                               password=password,
+    #                               host=host,
+    #                               port=port,
+    #                               database=database)
+    connection = psycopg2.connect(user="postgres",
+                                  password='Slayer@45',
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="vishnusaketh")
     cursor = connection.cursor()
     create_db(cursor)
     connection.commit()
